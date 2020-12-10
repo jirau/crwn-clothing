00e59b5da71ef9285681b0c5f7a316dc8badef09
Ariel Jirau <jirau@us.ibm.com>
2020-12-10 13:12:44 -0500

    added shop data to firebase and added the code to implement the calls to bring the data to the respective pages

---
+++ src/components/collection-item/collection-item.styles.jsx
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+
+export const CollectionItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+`
